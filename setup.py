aa27fed97dbbc5161300cbe4016d8378c4704c10
Steve Avery <schavery@gmail.com>
2016-04-09 16:05:14 -0700

    Complete switch to py3. fix css uri handling

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
 	scripts=['wcsm'],
 	license='LICENSE.txt',
 	install_requires=[
-		"argparse >= 1.2.1",
-		"beautifulsoup4 >= 4.3.2",
-		"chardet >= 2.0.1",
-		"cssutils >= 1.0",
-		"requests >= 2.2.1",
-		"watchdog >= 0.7.1",
-		"html5lib >= 0.999999"
+		"argparse >= 1.4.0",
+		"beautifulsoup4 >= 4.4.1",
+		"chardet >= 2.3.0",
+		"cssutils >= 1.0.1",
+		"requests >= 2.9.1",
+		"watchdog >= 0.8.3",
+		"lxml >= 3.6.0"
 	]
 )
 
